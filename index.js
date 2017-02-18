4ddd037764937dc060c3ec8141d7888f4766e46a
Matthew Bauer <mjbauer95@gmail.com>
2017-02-18 07:45:11 -0600

    Use cookies instead of qstring.
    should be more secure

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 express = require('express')
 app = express()
 scraper = require('./scraper')
-bodyParser = require('body-parser');
+bodyParser = require('body-parser')
+cookieParser = require('cookie-parser')
 
 app.use(express.static('static'))
 app.use(bodyParser())
+app.use(cookieParser())
 
 app.get('/ical', function (req, res) {
     ical(req, res)
@@ -15,8 +17,8 @@ app.post('/ical', function (req, res) {
 })
 
 function ical (req, res) {
-    userid = req.body.userid || req.query.userid
-    pwd = req.body.pwd || req.query.pwd
+    userid = req.body.userid || req.query.userid || req.cookies["userid"]
+    pwd = req.body.pwd || req.query.pwd || req.cookies["pwd"]
     scraper.getPage(userid, pwd).then(function(body) {
         return scraper.parseStr(req.body.userid, body).then(function(r) {
             res.header("Content-Type", "text/calendar")
@@ -28,6 +30,33 @@ function ical (req, res) {
         console.log(err)
     })
 }
+
+app.post('/calendar', function (req, res) {
+    res.cookie("userid", req.body.userid)
+    res.cookie("pwd", req.body.pwd)
+    res.sendFile(__dirname + "/static/calendar.html")
+})
+
+app.get('/calendar', function (req, res) {
+    res.sendFile(__dirname + "/static/calendar.html")
+})
+
+app.get('/logout', function (req, res) {
+    res.cookie("userid", "")
+    res.cookie("pwd", "")
+    res.redirect("/login")
+})
+
+app.get('/login', function (req, res) {
+    res.sendFile(__dirname + "/static/login.html")
+})
+
+app.get('/', function (req, res) {
+    if (!req.cookies["userid"] || req.cookies["userid"] == "")
+        res.redirect("/login")
+    else
+        res.redirect("/calendar")
+})
 
 port = process.env.PORT || 8000
 app.listen(port, function () {
