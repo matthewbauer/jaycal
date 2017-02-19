dc88584080a8fb259134f0c882e9078967185804
Matthew Bauer <mjbauer95@gmail.com>
2017-02-18 18:04:12 -0600

    cleanups

--- static/calendar.js
+++ static/calendar.js
@@ -1,33 +1,3 @@
-function hash_code(string) {
-    h = 0
-    for (i = 0; i < string.length; i++) {
-        h = string.charCodeAt(i) + ((h << 5) - h);
-    }
-    return h
-}
-
-function hash_color(string) {
-    return 'hsl('+hash_code(string)%360+', 70%, 50%)'
-}
-
-function colorize_event(e) {
-    for (c of e.classList) {
-       if (c.startsWith('event-')) {
-           e.style['background'] = e.style['border-color'] = hash_color(c)
-       }
-    }
-}
-
-function colorize_events() {
-    console.log("colorize")
-    ee = document.getElementsByClassName('fc-event')
-    for (i in ee) {
-        try {
-            colorize_event(ee[i])
-        } catch (TypeError) {}
-    }
-}
-
 function getCookie(cname) {
     var name = cname + "="
     var decodedCookie = decodeURIComponent(document.cookie)
@@ -63,7 +33,6 @@ $(document).ready(function() {
         if (req.status === 200) {
             $('#calendar').fullCalendar('addEventSource', fc_events(this.response, {}))
             $('#calendar').fullCalendar('addEventSource', expand_recur_events)
-//            colorize_events()
          } else if (req.status == 0) {
          } else {
              window.location.href = "/logout"
