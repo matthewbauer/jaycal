f5ea54220b96ea335c7e8911e4ea91eccea5e5fd
Matthew Bauer <mjbauer95@gmail.com>
2017-02-21 11:54:00 -0600

    Update iCal to make less events.

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ if (process.env.PORT)
 app.get('/schedule.ics', function (req, res) {
     userid = req.body.userid || req.query.userid || req.cookies["userid"]
     pwd = req.body.pwd || req.query.pwd || req.cookies["pwd"]
-    key = userid + pwd + "3"
+    key = userid + pwd + "4"
     client.get(key, function (userid, pwd, key, err, reply) {
         if (reply != null) {
             res.header("Content-Type", "text/calendar")
--- scraper.js
+++ scraper.js
@@ -60,23 +60,23 @@ function getDays(dates) {
     for (var i = 0; i < days.length; i++) {
         if (days[i] == "T") {
             if (days[i+1] == "u") {
-                ds.push("Tuesday")
+                ds.push("TU")
             } else {
-                ds.push("Thursday")
+                ds.push("TH")
             }
             i++
         } else if (days[i] == "S"){
             if (days[i+1] == "u") {
-                ds.push("Sunday")
+                ds.push("SU")
             } else {
-                ds.push("Saturday")
+                ds.push("SA")
             }
             i++
         } else {
             ds.push({
-                "M": "Monday",
-                "W": "Wednesday",
-                "F": "Friday"
+                "M": "MO",
+                "W": "WE",
+                "F": "FR"
             }[days[i]]);
         }
     }
@@ -94,80 +94,86 @@ function hasSection(obj) {
 function toJCal(obj, id) {
     email = id + "@ku.edu"
     events = [].concat.apply([], obj.map(function(c) {
-        return [].concat.apply([], c.sections.map(function(s) {
+        return c.sections.map(function(s) {
             days = getDays(s.times)
-            return days.map(function(day) {
-                summary = c.name.split(" - ")[0]
-                description = c.name +
-                    "\nsection number: " + s.number +
-                    "\nunits: " + c.units
-                if (s.room)
-                    description += "\nroom number: " + s.room
-                if (s.instructors)
-                    description += "\ninstructor: " + s.instructors.join(", ")
+            summary = c.name.split(" - ")[0]
+            description = c.name +
+                "\nsection number: " + s.number +
+                "\nunits: " + c.units
+            if (s.room)
+                description += "\nroom number: " + s.room
+            if (s.instructors)
+                description += "\ninstructor: " + s.instructors.join(", ")
 
-                start_time = s.times.split(" ")[1]
+            start_time = s.times.split(" ")[1]
 
-                end_time = s.times.split(" ")[3]
+            end_time = s.times.split(" ")[3]
 
-                start = new Date(s.dates.split(" - ")[0])
+            start = new Date(s.dates.split(" - ")[0])
 
-                currentDay = start.getDay();
-                distance = {
-                    "Sunday": 0,
-                    "Monday": 1,
-                    "Tuesday": 2,
-                    "Wednesday" : 3,
-                    "Thursday": 4,
-                    "Friday": 5,
-                    "Saturday": 6
-                }[day] - currentDay;
-                if (distance < 0) { // monday fix
-                    distance += 7
-                }
-                start.setDate(start.getDate() + distance);
+            currentDay = start.getDay();
+            distance = {
+                "SU": 0,
+                "MO": 1,
+                "TU": 2,
+                "WE" : 3,
+                "TH": 4,
+                "FR": 5,
+                "SA": 6
+            }[days[0]] - currentDay;
+            if (distance < 0) { // monday fix
+                distance += 7
+            }
+            start.setDate(start.getDate() + distance);
 
-                start_hours = parseInt(start_time.split(":")[0])
-                if (start_time.split(":")[1].endsWith("PM") && !start_time.startsWith("12")) {
-                    start_hours += 12
-                }
-                start.setHours(start_hours)
-                start.setMinutes(start_time.split(":")[1].slice(0, 2))
+            start_hours = parseInt(start_time.split(":")[0])
+            if (start_time.split(":")[1].endsWith("PM") && !start_time.startsWith("12")) {
+                start_hours += 12
+            }
+            start.setHours(start_hours)
+            start.setMinutes(start_time.split(":")[1].slice(0, 2))
 
-                start_end = new Date(start.getTime())
-                start_end_hours = parseInt(end_time.split(":")[0])
-                if (end_time.split(":")[1].endsWith("PM") && !end_time.startsWith("12")) {
-                    start_end_hours += 12
-                }
-                start_end.setHours(start_end_hours)
-                start_end.setMinutes(end_time.split(":")[1].slice(0, 2))
+            start_end = new Date(start.getTime())
+            start_end_hours = parseInt(end_time.split(":")[0])
+            if (end_time.split(":")[1].endsWith("PM") && !end_time.startsWith("12")) {
+                start_end_hours += 12
+            }
+            start_end.setHours(start_end_hours)
+            start_end.setMinutes(end_time.split(":")[1].slice(0, 2))
 
-                end = new Date(s.dates.split(" - ")[1])
-                end.setDate(end.getDate() - 7)
+            end = new Date(s.dates.split(" - ")[1])
+            end.setDate(end.getDate() - 7)
 
-                subject = c.name.split(" - ")[0].replace(" ", "+")
-                url = "https://classes.ku.edu/Classes/CourseSearchAPI.action?classesSearchText=" + subject
+            subject = c.name.split(" - ")[0].replace(" ", "+")
+            url = "https://classes.ku.edu/Classes/CourseSearchAPI.action?classesSearchText=" + subject
 
-                return ["vevent",
-                        [
-                            ["dtstamp", {}, "date-time", (new Date()).toISOString()],
-                            ["dtstart", {}, "date-time", start.toISOString()],
-                            ["dtend", {}, "date-time", start_end.toISOString()],
-                            ["location", {}, "text", s.room],
-                            ["organizer", {}, "text", "mailto:" + email],
-                            ["uid", {}, "text", "" + Math.floor(Math.random() * Math.pow(10, 10))],
-                            ["summary", {}, "text", summary],
-                            ["description", {}, "text", description],
-                            ["rrule", {}, "recur", {
-                                "freq": "WEEKLY",
-                                "until": end.toISOString(),
-                            }],
-                            ["url", {}, "text", url]
-                        ],
-                        []
-                       ]
-            })
-        }))
+            uid = ""
+            if (s.number == " ") {
+                uid = Math.floor(Math.random() * Math.pow(10, 10)) + "@jaycal.herokuapp.com"
+            } else {
+                uid = s.number + "@jaycal.herokuapp.com"
+            }
+
+            return ["vevent",
+                    [
+                        ["dtstamp", {}, "date-time", (new Date()).toISOString()],
+                        ["dtstart", {}, "date-time", start.toISOString()],
+                        ["dtend", {}, "date-time", start_end.toISOString()],
+                        ["location", {}, "text", s.room],
+                        ["organizer", {}, "text", "mailto:" + email],
+                        ["uid", {}, "text", uid],
+                        ["summary", {}, "text", summary],
+                        ["description", {}, "text", description],
+                        ["rrule", {}, "recur", {
+                            "freq": "WEEKLY",
+                            "byday": days.join(","),
+                            "until": end.toISOString(),
+                        }],
+                        ["url", {}, "text", url],
+                    ],
+                    [],
+                   ]
+        })
     }))
     return ["vcalendar",
             [
